362957bac9af3c66aee40236fd2986b38b0f50e1
Alvin Ababil <neoryumasil@gmail.com>
2025-10-22 17:29:13 +0700

    Add Resources and Set Up Main Menu Looking

---
+++ Guess The Character NNM/Assets/Script/UI Script/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string _playScene;
+    [SerializeField] private string _optionScene;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(_playScene);
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene(_optionScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
